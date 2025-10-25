e92a8d77faac3243743ac6a3be963ad1681d33c0
Kirill Poselenov <luckykepos@gmail.com>
2025-10-25 15:34:38 +0400

    add new folders/Frontend to repo

---
+++ Frontend/coffien/js/modal.js
@@ -0,0 +1,19 @@
+let modalBtn = document.getElementById("modal");
+let modalActive = document.getElementById("content-modal");
+let modalBtnClose = document.getElementById("close-modal");
+
+function openModal() {
+
+    modalActive.classList.add("active");
+
+
+}
+
+function closeModal() {
+
+    modalActive.classList.remove("active");
+
+}
+
+modalBtn.addEventListener("click", openModal);
+modalBtnClose.addEventListener("click", closeModal);
