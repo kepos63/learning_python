14f2ed8edcbbba4932fe326b3b507bf8d336109d
Kirill Poselenov <luckykepos@gmail.com>
2025-10-25 15:28:41 +0400

    refactorind repo

--- lesson_9.py
+++
@@ -1,6 +0,0 @@
-import aiogram
-
-pip install aiogram # в терминале написать
-pip install pyTelegrambotAPI
-
-https://www.python.org/  # Документация; описание: API, модули, библиотеки;
--- module.py
+++
@@ -1,7 +0,0 @@
-#
-
-url = 'DateBases/university.bd'
-name = "university"
-
-def hello():
-    print('Добрый вечер!\n Спасибо, что используете нашу базу данных')
